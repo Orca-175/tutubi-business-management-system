69ef470bad4e869cc6dec26d3a5a46b909bfcfa8
Orca-175 <stephorcaa@gmail.com>
2026-09-19 17:24:51 +0800

    Style AdminLogin component

--- src/admin-routes/authentication/AdminLogin.tsx
+++ src/admin-routes/authentication/AdminLogin.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import { auth } from '../../firebase/firebase';
+import common from '../../styles/common-styles/Common.module.scss';
+import styles from './AdminLogin.module.scss';
 
 export function AdminLogin() {
   const [email, setEmail] = useState('');
@@ -11,31 +13,40 @@ export function AdminLogin() {
   const navigate = useNavigate();
 
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
+    <div className={common.root}>
+      <div className={styles.card}>
+        <h1 className={styles.heading}>Admin Login</h1>
 
-        await signInWithEmailAndPassword(auth, email, password);
-        navigate('/admin');
-      }}
-    >
-      <h1>Admin Login</h1>
+        <form
+          className={styles.authForm}
+          onSubmit={async (event) => {
+            event.preventDefault();
 
-      <label htmlFor="username">Username:</label>
-      <input
-        id="username"
-        onChange={(event) => setEmail(event.target.value)}
-        type="text"
-      />
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate('/admin');
+          }}
+        >
+          <div className={styles.formInput}>
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              onChange={(event) => setEmail(event.target.value)}
+              type="text"
+            />
+          </div>
 
-      <label htmlFor="password">Password:</label>
-      <input
-        id="password"
-        onChange={(event) => setPassword(event.target.value)}
-        type="password"
-      />
+          <div className={styles.formInput}>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+            />
+          </div>
 
-      <button type="submit">Login</button>
-    </form>
+          <button className={styles.formButton} type="submit">Login</button>
+        </form>
+      </div>
+    </div>
   );
 }
--- src/admin-routes/home/Home.tsx
+++ src/admin-routes/home/Home.tsx
@@ -1,3 +1,19 @@
+import { signOut } from 'firebase/auth';
+
+import { auth } from '../../firebase/firebase';
+
 export function AdminHome() {
-  return <div>This is the admin home component.</div>;
+  return (
+    <div>
+      This is the admin home component.
+    <button
+      onClick={(event) => {
+        event.preventDefault();
+        signOut(auth);
+      }}
+    >
+      Logout
+    </button>
+    </div>
+  );
 }
