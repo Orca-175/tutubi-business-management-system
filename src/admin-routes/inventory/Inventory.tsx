b663f6111fccefc51d9535ebe3660f0a1f20636c
Orca-175 <stephorcaa@gmail.com>
2026-09-23 18:53:56 +0800

    Establish initial common page styles

--- src/admin-routes/inventory/Inventory.tsx
+++ src/admin-routes/inventory/Inventory.tsx
@@ -1,5 +1,35 @@
+import common from '../../styles/common-styles/Common.module.scss';
+
 export function Inventory() {
   return (
-    <div>This is the inventory component.</div>
+    <div className={common.root}>
+      <h2>View Inventory</h2>
+      <hr />
+      <div className={common.card}>
+        <div className={common.tableContainer}>
+          <table>
+            <thead>
+              <tr>
+                <th>Field</th>
+                <th>Field</th>
+                <th>Field</th>
+                <th>Field</th>
+                <th>Field</th>
+                <th>Field</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Value</td>
+                <td>Value</td>
+                <td>Value</td>
+                <td>Value</td>
+                <td>Value</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 }
