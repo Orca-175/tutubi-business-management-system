5dc1b79ca6ecafb986f7cfb5deb3bb09c1950fef
Orca-175 <stephorcaa@gmail.com>
2026-09-23 19:17:46 +0800

    Fix broken styles for AdminLogin component

--- src/admin-routes/authentication/AdminLogin.tsx
+++ src/admin-routes/authentication/AdminLogin.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import { auth } from '../../firebase/firebase';
-import common from '../../styles/common-styles/Common.module.scss';
+import authentication from '../../styles/common-styles/Authentication.module.scss';
 import styles from './AdminLogin.module.scss';
 
 export function AdminLogin() {
@@ -13,7 +13,7 @@ export function AdminLogin() {
   const navigate = useNavigate();
 
   return (
-    <div className={common.root}>
+    <div className={authentication.root}>
       <div className={styles.card}>
         <h1 className={styles.heading}>Admin Login</h1>
 
